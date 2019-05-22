060c5686ba97d9b96569f03bacaa853d4a35d49f
Alejandro Francisco García Uclés <agu528@inlumine.ual.es>
2019-05-22 20:43:17 +0200

    Todos los tests pasando.

--- junitComplejo/test/junitComplejo/AllTests.java
+++ junitComplejo/test/junitComplejo/AllTests.java
@@ -1,12 +1,11 @@
 package junitComplejo;
 
-
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ ComplejoTest.class, ComplejoTest2.class, ComplejoTestParametrized.class })
+@SuiteClasses({ ComplejoTest.class, Complejo2Test.class, ComplejoParametrizedTest.class })
 public class AllTests {
 
 	
